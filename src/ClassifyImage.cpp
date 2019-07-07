911c1f8e3b7b6455bec806a79054de3ce3b000d7
Andrew Schneider <aschne93@gmail.com>
2019-07-07 15:32:25 -0700

    Color selection based on HSV values
     - broke contour counting

--- src/ClassifyImage.cpp
+++ src/ClassifyImage.cpp
@@ -3,21 +3,43 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
+cv::Scalar min_thresh [3] = {cv::Scalar(0, 100, 100),   // red
+                            cv::Scalar(60, 100, 100),   // green
+                            cv::Scalar(100, 50, 100)};  // blue
 
-// Image classifier to determine color, shape, and count.
+cv::Scalar max_thresh [3] = {cv::Scalar(20, 255, 255),
+                            cv::Scalar(80, 255, 255),
+                            cv::Scalar(120, 150, 255)};
+
+std::string colors[3] = {"red", "green", "blue"};
+
+// Image classifier to determine color, shape, fill, and count.
 cv::Mat ClassifyImage(cv::Mat inputImage) {
     // Convert to greyscale
-    cv::Mat greyImage;
-    cv::cvtColor(inputImage, greyImage, cv::COLOR_BGR2GRAY);
+    // cv::Mat greyImage;
+    // cv::cvtColor(inputImage, greyImage, cv::COLOR_BGR2GRAY);
+
+    cv::Mat hsvImage;
+    cv::cvtColor(inputImage, hsvImage, cv::COLOR_BGR2HSV);
 
     // Threshold
-    // cv::Mat threshImage;
-    // cv::threshold(greyImage, threshImage, 160, 255, cv::THRESH_BINARY);
+    cv::Mat threshImage;
+    for (int i = 0; i < 3; i++ ){
+        cv::inRange(hsvImage, min_thresh[i], max_thresh[i], threshImage);
+
+        double coloredPixels = cv::countNonZero(threshImage);
+        // std::cout << colors[i] << " non zero pixels: " << coloredPixels << std::endl;
+    
+        if ( coloredPixels > 0 ){
+          std::cout << " +++++ Color is: " << colors[i] << std::endl;
+          break;
+        }
+    }
 
     // Determine edges using Canny
     cv::Mat cannyImage;
     // cv::floodFill(greyImage, cv::Point(0, 0), cv::Scalar(0));
-    cv::Canny(greyImage, cannyImage, 140, 250, 3);
+    cv::Canny(threshImage, cannyImage, 140, 250, 3);
 
     // Find contours
     std::vector<std::vector<cv::Point> > contours;
@@ -28,7 +50,7 @@ cv::Mat ClassifyImage(cv::Mat inputImage) {
     std::cout << "Number of shapes: " << contours.size() << std::endl;
 
     // Draw contours
-    cv::Mat drawing = cv::Mat::zeros( greyImage.size(), CV_8UC3 );
+    cv::Mat drawing = cv::Mat::zeros( hsvImage.size(), CV_8UC3 );
     for (int i =0; i < contours.size(); i++ ){
         cv::drawContours(drawing, contours, i, CV_RGB(0, 0, 255), 2, 8,
                         hierarchy, 0);
