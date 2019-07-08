c1ed9233e2dad48c14be8be27073d1ed5934d63d
Andrew Schneider <aschne93@gmail.com>
2019-07-07 22:15:05 -0700

    Fixed contour counting
    - switched to thresholded image instead of using Canny

--- src/ClassifyImage.cpp
+++ src/ClassifyImage.cpp
@@ -5,11 +5,11 @@
 
 cv::Scalar min_thresh [3] = {cv::Scalar(0, 100, 100),   // red
                             cv::Scalar(60, 100, 100),   // green
-                            cv::Scalar(100, 50, 100)};  // blue
+                            cv::Scalar(110, 50, 100)};  // blue
 
 cv::Scalar max_thresh [3] = {cv::Scalar(20, 255, 255),
                             cv::Scalar(80, 255, 255),
-                            cv::Scalar(120, 150, 255)};
+                            cv::Scalar(130, 150, 255)};
 
 std::string colors[3] = {"red", "green", "blue"};
 
@@ -37,26 +37,27 @@ cv::Mat ClassifyImage(cv::Mat inputImage) {
     }
 
     // Determine edges using Canny
-    cv::Mat cannyImage;
+    // cv::Mat cannyImage;
     // cv::floodFill(greyImage, cv::Point(0, 0), cv::Scalar(0));
-    cv::Canny(threshImage, cannyImage, 140, 250, 3);
+    // cv::Canny(threshImage, cannyImage, 140, 250, 3);
 
     // Find contours
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(cannyImage, contours, hierarchy, CV_RETR_EXTERNAL,
+    cv::findContours(threshImage, contours, hierarchy, CV_RETR_EXTERNAL,
                     CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
-    std::cout << "Number of shapes: " << contours.size() << std::endl;
+    std::cout << " +++++ Number of shapes: " << contours.size() << std::endl;
 
     // Draw contours
-    cv::Mat drawing = cv::Mat::zeros( hsvImage.size(), CV_8UC3 );
-    for (int i =0; i < contours.size(); i++ ){
-        cv::drawContours(drawing, contours, i, CV_RGB(0, 0, 255), 2, 8,
-                        hierarchy, 0);
-        // double area = cv::contourArea(contours[i], false);
-        // std::cout << "Contour area: " << area << std::endl;
+    cv::Mat contourOverlay = threshImage.clone();
+    cv::cvtColor(contourOverlay, contourOverlay, cv::COLOR_GRAY2RGB);
+    for (int i = 0; i < contours.size(); i++) {
+      cv::drawContours(contourOverlay, contours, i, CV_RGB(0, 0, 255), 2, 8,
+                       hierarchy, 0);
+      // double area = cv::contourArea(contours[i], false);
+      // std::cout << "Contour area: " << area << std::endl;
     }
 
-    return drawing;
+    return contourOverlay;
 }
